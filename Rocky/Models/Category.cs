f046a77107139c343a249a5e4c8a9bd2aeca19a1
MambaJMR <MambaJMR@users.noreply.github.com>
2023-04-02 19:12:59 +0300

    RockShop

---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Display order for category must be greater than 0")]
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ Rocky/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rocky.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Название продукта")]
+        public string Name { get; set; }
+        [Display(Name = "Краткое описание")]
+        public string ShortDesc { get; set; }
+        [Display(Name = "Описание продукта")]
+        public string Description { get; set; }
+        [Range(1, int.MaxValue)]
+        [Display(Name = "Цена")]
+        public int Price { get; set; }
+        public string Image { get; set; }
+
+        [Display(Name = "Категория")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+
+        public virtual Category Category { get; set; }
+        [Display(Name = "Тип операции")]
+        public int ApplicationTypeId { get; set; }
+
+        [ForeignKey("ApplicationTypeId")]
+        public virtual ApplicationType ApplicationType { get; set; }
+    }
+}
